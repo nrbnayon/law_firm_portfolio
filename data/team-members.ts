817592ad8c90236adbfcfb82b8760cc99c1bbda2
nrbnayon <nrbnayon@gmail.com>
2025-10-18 16:41:01 +0600

    fix

--- data/team-members.ts
+++ data/team-members.ts
@@ -96,7 +96,7 @@ export const teamMembers: TeamMember[] = [
     id: "jack",
     name: "Jack",
     role: "Lawyer",
-    image: "/team-member-2.png",
+    image: "/team-member-3.png",
     backgroundImage: "/attorney.png",
     description:
       "Lorem ipsum dolor sit amet consectetur. Quam mattis tristique id eras odio leo. Condimentum tristique etiam amet tincidunt viverra. Est amet arcu eu commodo tempus lorem. Rhoncus ultrices imperdiet et velit suspendisse mi. Sit lectus vel aliquam sit faucibus. Elit sed placerat urna feugiat sollicitudin.",
